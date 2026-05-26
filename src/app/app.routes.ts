378ee9efbcc8fd82ddc6d2f342648e08ba6857f9
Manasi788 <manasimadhu18@gmail.com>
2026-05-26 13:05:27 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { About } from './page/about/about';
+import { Contact } from './page/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',component:Home
+    },
+    {
+        path:'about',component:About
+    },
+    {
+        path:'contact',component:Contact
+    }
+
+];
